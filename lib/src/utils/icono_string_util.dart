4ccd020184de4ccce7d867b3c0083850fdfc55f7
Carlos Haupter Pérez <haupterperezcarlos@gmail.com>
2021-10-15 14:43:00 +0200

    Añadido animatedContainer

--- lib/src/utils/icono_string_util.dart
+++ lib/src/utils/icono_string_util.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 //Poner siempre initialData para que nos funciones con el null safety
 final _icons = <String, IconData>{
-  'add.alert': Icons.add_alert,
+  'add_alert': Icons.add_alert,
   'accessibility': Icons.accessibility,
   'folder_open': Icons.folder_open,
+  'donut_large': Icons.donut_large,
 };
 
 Icon getIcon(String nombreIcono) {
